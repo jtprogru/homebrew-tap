5ea5e39c3f8be60b2131b60dcc1c97b0e47616a2
Mikhail Savin <jtprogru@gmail.com>
2026-06-20 18:59:50 +0000

    Update asphyxia to 0.2.2

--- Formula/asphyxia.rb
+++ Formula/asphyxia.rb
@@ -6,18 +6,18 @@ class Asphyxia < Formula
   on_macos do
     on_arm do
       url "https://github.com/jtprogru/asphyxia/releases/download/0.2.2/asphyxia-aarch64-apple-darwin.zip"
-      sha256 "5189d49dfe0e9a5925ed6bc4d9d2e82ce26586ad478e7ae8762f4853a94011f7"
+      sha256 "bc8f51b7efad55e2f90739df40af4426b1662d51773e874ac90de2eec43d8df5"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/jtprogru/asphyxia/releases/download/0.2.2/asphyxia-x86_64-unknown-linux-gnu.zip"
-      sha256 "13677cd24c10c9d7f9323bb6c49ee5718458dee66b4f828524a6611f30f2028b"
+      sha256 "566c23f9bfc6e98088c432729ca337b9cdf0e2cdc1db0498b65b919f204830d2"
     end
     on_arm do
       url "https://github.com/jtprogru/asphyxia/releases/download/0.2.2/asphyxia-aarch64-unknown-linux-gnu.zip"
-      sha256 "cf918015ef711a2cd78107460a1e09782f7f95585d2a526702fd6466e3553a39"
+      sha256 "162100c3a78a0c0bb04e1e9a60aa6c94cbac0035351d46f177a2e4dd2382a413"
     end
   end
 
