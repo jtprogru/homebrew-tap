22deef54792b1295e82b43b30b0c60af34db4c71
Mikhail Savin <jtprogru@gmail.com>
2026-07-04 12:50:40 +0000

    Update asphyxia to 0.7.1

--- Formula/asphyxia.rb
+++ Formula/asphyxia.rb
@@ -1,23 +1,23 @@
 class Asphyxia < Formula
   desc "A fast and efficient network scanner written in Rust"
   homepage "https://github.com/jtprogru/asphyxia"
-  version "0.7.0"
+  version "0.7.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.0/asphyxia-aarch64-apple-darwin.zip"
-      sha256 "e998258d8c4e087e07017c4bebfdf92f2de42560d4c0cb2d64db1f563990b562"
+      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.1/asphyxia-aarch64-apple-darwin.zip"
+      sha256 "b6454a477ce7e9db50dd814eaa84be894c7e8bc9ce3c2f2dffe17648891a6428"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.0/asphyxia-x86_64-unknown-linux-gnu.zip"
-      sha256 "561caf7d0a91928c7522ab30f2ab506b50b9c0ff1584f84b7ab53cea3787e971"
+      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.1/asphyxia-x86_64-unknown-linux-gnu.zip"
+      sha256 "ea569195e083d9860dff2383dc35f195bdf2225b2596c561f3ace513b389bd12"
     end
     on_arm do
-      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.0/asphyxia-aarch64-unknown-linux-gnu.zip"
-      sha256 "f4ca52cd58c9d331e2cb02435792de7147b7097dbdaf1bb6f8ff02afa6bbe52c"
+      url "https://github.com/jtprogru/asphyxia/releases/download/0.7.1/asphyxia-aarch64-unknown-linux-gnu.zip"
+      sha256 "4fc52fd50ddd704536ba3a50eb9ff5ab8144d593eaf74f885f16b1260c74f7e4"
     end
   end
 
